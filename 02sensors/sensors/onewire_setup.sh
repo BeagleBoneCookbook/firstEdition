dd8290949d7ca83209e3e6e2676e5a0216bc2a34
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-05-20 12:38:29 -0400

    Removed extra nested directories

--- 02sensors/sensors/onewire_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Compile it
-dtc -O dtb -o BB-W1-00A0.dtbo -b 0 -@ BB-W1-00A0.dts
-# Copy it
-cp BB-W1-00A0.dtbo /lib/firmware/
-# Enable it:
-echo BB-W1 > /sys/devices/bone_capemgr.*/slots
